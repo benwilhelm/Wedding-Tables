ee7eb59d6781818f7fff85b89dc4981df5ca1653
Ben Wilhelm <ben@doublebeamdesign.com>
2012-10-12 10:29:49 -0500

    UI improvements
    * hover state for receiving table
    * warning if over 10 people per table
    * better differentiation of drop target

--- assets/js.js
+++ assets/js.js
@@ -7,6 +7,20 @@ $(document).ready(function(){
       var params = {'table_id':tid,'bodies':bds} ;
       console.log(params) ;
       $.post('update-table-ids.php',params) ;
+      
+      var count = $(this).find('li').length ;
+      if (count > 10) {
+        $(this).addClass('overloaded') ;
+      } else {
+        $(this).removeClass('overloaded') ;
+      }
+    },
+    
+    over: function(event,ui) {
+      $(this).addClass('hover') ;
+    },
+    out: function(event,ui) {
+      $(this).removeClass('hover') ;
     }
   }).disableSelection() ;
 }) ;
