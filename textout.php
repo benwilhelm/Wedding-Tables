cfc533cced1bbec6e439de13ccf99bf3b2908483
Ben Wilhelm <ben@doublebeamdesign.com>
2012-10-15 07:11:30 -0700

    timestamp at top of printout

--- textout.php
+++ textout.php
@@ -6,6 +6,8 @@ header('Content-Disposition: attachment; filename: "wedding-tables.txt";');
 require_once "include/db-connect.php" ;
 require_once "include/get-tables.php" ;
 
+echo "Printed on " . date("F j") . " at " . date("g:i a") . "\n\n" ;
+
 foreach ($tables as $table) {
   echo "\n{$table['table_name']}\n" ;
   echo "================================\n" ;
